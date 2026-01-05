404d0a4f2b626d5c97b27beff740a8b3738f83f9
peanutdoughnut <cjkwanthe@gmail.com>
2026-01-05 02:55:20 +0200

    added js code added css code

--- myscript.js
+++ myscript.js
@@ -1,1 +1,41 @@
-This is an Etch-a-sketch project created using javascript, CSS and HTML inorder to practice DOM manipulation.
+const grid = document.querySelector(".gridContainer");
+const userInput = document.querySelector("quantity");
+const resetButton = document.querySelector(".reset")
+
+createGrid = () => {
+    for(let i=0; i<256; i++){
+        const div = document.createElement("div");
+        div.classList.add("square");
+        grid.appendChild(div);
+    }
+};
+
+updateGrid = () => {
+    grid.innerHTML ="";
+    grid.Style.setProperty("grid-template-columns",`repeat(${userInput.value},sfr)`);
+    grid.style.setProperty("grid-template-rows",`repeat(${userInput.value},2fr)`);
+
+    for(i=0; i<userInput.Value*userInput.value; i++){
+        const div = document.createElement("div")
+        div.classList.add("square");
+        grid.appendChild(div);
+    }
+    console.log(userInput.value);
+
+};
+
+const square = document.querySelector("div");
+square.addEventListener("mouseover" ,function(event){
+    event.target.classList.replace("square","color");
+});
+
+userInput.addEventListener("click", function(){
+    grid.innerHTML="";
+    userInput.value="";
+    grid.style.setProperty("grid-template-columns", `repeat(16, 2fr)`);
+    grid.style.setProperty("grid-template-row", `repeat(16,2fr)`);    
+    createGrid();
+});
+
+createGrid();
+
